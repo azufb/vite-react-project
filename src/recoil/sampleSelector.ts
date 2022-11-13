6200aaee34ecf0fb5645c4f12ce5a1a8fa84af2a
azufb <curiousazunas@gmail.com>
2022-11-13 17:49:07 +0900

    Selectorコメントアウト追加

--- src/recoil/sampleSelector.ts
+++ src/recoil/sampleSelector.ts
@@ -12,16 +12,20 @@ export const sampleSelector = selector({
 
 export const sampleSelector2 = selector({
     key: 'sampleSelector2',
+    // get関数は必須。
     get: ({ get }) => {
         return get(sampleState2);
     },
+    // set関数は、オプション。
     set: ({ get, set }, newValue: any) => {
         const getState = get(sampleState2);
+
         const newItem = {
             item: newValue,
             done: false
         };
 
+        // 第1引数は、更新したいAtomの現在の状態。第2引数は、新しい値。
         set(sampleState2, [...getState, newItem]);
     }
 });
