e0fc087e21d9a7c8a8b63ccffdbd9f4b8a087de1
azufb <curiousazunas@gmail.com>
2022-11-15 00:07:58 +0900

    タスクを表示できるようにした

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,16 @@
+import { useRecoilValue } from "recoil";
+import { allTasksAtom, allTasksAtomType } from '../recoil/recoilState';
+
 const List = () => {
+    const allTasks = useRecoilValue<allTasksAtomType>(allTasksAtom);
     return (
         <div>
             <h1>リスト</h1>
+            {allTasks.map((task: any, index: number) => (
+                <div key={index}>
+                    <p>{task.taskTitle}</p>
+                </div>
+            ))}
         </div>
     );
 };
