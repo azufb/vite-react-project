1fe9a1baf44604373280e1f6ff04c0165cfcae5b
azufb <curiousazunas@gmail.com>
2022-11-14 22:12:00 +0900

    元々作っていたページの要素削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { sampleState, sampleState2 } from './recoil/sampleAtom';
-import { sampleSelector, sampleSelector2 } from './recoil/sampleSelector';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const sampleVal = useRecoilValue(sampleState);
-  const sampleSel = useRecoilValue(sampleSelector);
-  const setSmpleSt2 = useSetRecoilState(sampleSelector2);
-  const sample2Val = useRecoilValue(sampleState2);
-
-
+const App = () => {
   return (
     <div className="App">
-      <p>Atom: {sampleVal}</p>
-      <p>Selector: {sampleSel}</p>
-      <p>sample2Val：</p>
-      {sample2Val.map((val, index) => (
-        <p key={index}>{val.item}</p>
-      ))}
-      {sampleVal.map((val, index) => (
-        <button onClick={() => setSmpleSt2(val)} key={index}>{val}</button>
-      ))}
+      <h1>Todo List</h1>
     </div>
   )
 }
