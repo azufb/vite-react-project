455398a97fcfb4f25951e6a54ee7731e25eec593
azufb <curiousazunas@gmail.com>
2022-11-14 23:35:54 +0900

    React Hook Formでフォーム作成

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -1,9 +1,17 @@
+import { useForm } from "react-hook-form";
+
 const AddForm = () => {
+    const { register, handleSubmit } = useForm();
+
+    const addTask = (data: any) => {
+        console.log(data);
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(addTask)}>
             <label htmlFor="task">タスク：</label>
-            <input id="task" />
-            <button>ADD</button>
+            <input id="task" {...register('task')} />
+            <button type='submit'>ADD</button>
         </form>
     );
 };
