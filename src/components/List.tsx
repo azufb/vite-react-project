79fb9e346c58520cf210b0f5bd2ba27ae02d8abb
azufb <curiousazunas@gmail.com>
2022-11-16 14:34:35 +0900

    修正

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,15 +1,23 @@
-import { useRecoilValue } from "recoil";
-import { allTasksAtom, allTasksAtomType } from '../recoil/recoilState';
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { allTasksAtom, allTasksAtomType, changeTaskEditableSelector } from '../recoil/recoilState';
+import EditForm from "./EditForm";
 
 const List = () => {
     const allTasks = useRecoilValue<allTasksAtomType>(allTasksAtom);
+    const setChangeTaskEditable = useSetRecoilState(changeTaskEditableSelector);
+
+    const changeEditable = (targetTask: any): void => {
+        setChangeTaskEditable(targetTask.id);
+    }
+
     return (
         <div>
-            <h1>リスト</h1>
             {allTasks.map((task: any, index: number) => (
                 <div key={index}>
-                    <p>{task.taskTitle}</p>
-                    <button>Edit</button>
+                    <span>No.{task.id}：</span>
+                    <span>{task.title}</span>
+                    <p>編集状態：{task.edit ? '可' : '不可'}</p>
+                    <button onClick={() => changeEditable(task)}>Edit</button>
                 </div>
             ))}
         </div>
