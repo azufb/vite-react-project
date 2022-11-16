0aa693cf05f98dccb4e13eb52f8ecb67010bc0e9
azufb <curiousazunas@gmail.com>
2022-11-16 21:02:29 +0900

    編集コンポーネントの表示切り替えを修正

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,43 +1,34 @@
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { modalStateAtom } from "../recoil/modalState";
-import { allTasksAtom, editTargetTaskAtom, allTasksAtomType, deleteTaskSelector } from '../recoil/recoilState';
+import { allTasksAtom, editTargetTaskAtom, allTasksAtomType, changeTaskEditableSelector, deleteTaskSelector } from '../recoil/recoilState';
 import EditForm from "./EditForm";
 
 const List = () => {
     const allTasks = useRecoilValue<allTasksAtomType>(allTasksAtom);
     const setDeleteTask = useSetRecoilState(deleteTaskSelector);
-    const [editTargetTask, setEditTargetTask] = useRecoilState(editTargetTaskAtom);
-    const [modalState, setModalState] = useRecoilState(modalStateAtom);
+    const setChangeTaskEditable = useSetRecoilState(changeTaskEditableSelector);
 
     const changeEditable = (targetTask: any): void => {
-        const targetTaskParam = {
-            id: targetTask.id,
-            title: targetTask.title
-        };
-        setEditTargetTask(targetTaskParam);
-
-        setModalState((currentState) => ({
-            ...currentState,
-            editTask: true
-        }));
+        setChangeTaskEditable(targetTask.id);
     }
 
     return (
         <div>
             {allTasks.map((task: any, index: number) => (
                 <div key={index}>
-                    <span>{task.title}</span>
-                    <button onClick={() => changeEditable(task)}>Edit</button>
-                    <button onClick={() => setDeleteTask(task)}>削除</button>
+                    {task.edit ? (
+                        <EditForm
+                        id={task.id}
+                        title={task.title}
+                    />
+                    ) : (
+                        <>
+                            <span>{task.title}</span>
+                            <button onClick={() => changeEditable(task)}>Edit</button>
+                            <button onClick={() => setDeleteTask(task)}>削除</button>
+                        </>
+                    )}
                 </div>
             ))}
-
-            {modalState.editTask && (
-                <EditForm
-                    id={editTargetTask.id}
-                    title={editTargetTask.title}
-                />
-            )}
         </div>
     );
 };
