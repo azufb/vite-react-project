aaf8cc20887b62b5a69d1c1b4c4ad875db3bf289
azufb <curiousazunas@gmail.com>
2022-11-17 15:35:04 +0900

    タスクの完了状態を切り替えるボタン実装

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,12 @@
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { allTasksAtom, editTargetTaskAtom, allTasksAtomType, changeTaskEditableSelector, deleteTaskSelector } from '../recoil/recoilState';
+import { allTasksAtom, editTargetTaskAtom, allTasksAtomType, changeTaskEditableSelector, deleteTaskSelector, changeTaskIsCompleted} from '../recoil/recoilState';
 import EditForm from "./EditForm";
 
 const List = () => {
     const allTasks = useRecoilValue<allTasksAtomType>(allTasksAtom);
     const setDeleteTask = useSetRecoilState(deleteTaskSelector);
     const setChangeTaskEditable = useSetRecoilState(changeTaskEditableSelector);
+    const setChangeTaskIsCompleted = useSetRecoilState(changeTaskIsCompleted);
 
     const changeEditable = (targetTask: any): void => {
         setChangeTaskEditable(targetTask.id);
@@ -24,7 +25,7 @@ const List = () => {
                         <>
                             <span>NO.{task.id}：</span>
                             <span>{task.title}</span>
-                            <span>{task.isCompleted ? 'DONE' : 'TODO'}</span>
+                            <button onClick={() => setChangeTaskIsCompleted(task)}>{task.isCompleted ? 'DONE' : 'TODO'}</button>
                             <button onClick={() => changeEditable(task)}>Edit</button>
                             <button onClick={() => setDeleteTask(task)}>削除</button>
                         </>
