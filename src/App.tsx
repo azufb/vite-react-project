5edc524dc09ea2bcb3e930bafe74ddbdc2020b3b
azufb <curiousazunas@gmail.com>
2022-11-17 16:08:21 +0900

    完了したタスクを表示できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import AddForm from './components/AddForm';
 import List from './components/List';
+import CompletedList from './components/CompletedList';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <h1>Todo List</h1>
       <AddForm />
       <List />
+      <CompletedList />
     </div>
   );
 };
