38111d48d727e07a1fc8faea6f329f6463a990ed
azufb <curiousazunas@gmail.com>
2022-11-19 22:34:13 +0900

    Statsコンポーネント作成

---
+++ src/components/Stats.tsx
@@ -0,0 +1,20 @@
+import { useRecoilValue } from "recoil";
+import { changeTasksStatsSelector, tasksStatsAtom, TasksStatsAtomType } from "../recoil/recoilState";
+
+const Stats = () => {
+    const tasksStats = useRecoilValue(changeTasksStatsSelector);
+
+    return (
+        <div>
+            <h1>タスク状況：</h1>
+            <ul>
+                <li>Total tasks: {tasksStats.allTasksCount}</li>
+                <li>Tasks Completed: {tasksStats.completedTasksCount}</li>
+                <li>Tasks Not Completed: {tasksStats.notCompletedTasksCount}</li>
+                <li>Percent Completed: {tasksStats.completedTasksPercentage}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Stats;
