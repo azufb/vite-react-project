2aaf1ffae5dc934a21d3e32991aec74846828919
azufb <curiousazunas@gmail.com>
2022-11-19 22:51:17 +0900

    Statsのセット方法を修正

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,17 +1,45 @@
-import { useRecoilValue } from "recoil";
-import { changeTasksStatsSelector } from "../recoil/recoilState";
+import { useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { allTasksAtom, AllTasksAtomType, TaskAtomType, tasksStatsAtom, TasksStatsAtomType } from "../recoil/recoilState";
 
 const Stats = () => {
-    const tasksStats = useRecoilValue(changeTasksStatsSelector);
+    const [stats, setStats] = useRecoilState<TasksStatsAtomType>(tasksStatsAtom);
+    const allTasks = useRecoilValue<AllTasksAtomType>(allTasksAtom);
+
+    useEffect(() => {
+        const allTasksCount: number = allTasks.length;
+        const completedTasks: AllTasksAtomType = allTasks.filter((task: TaskAtomType) => {
+            return task.isCompleted === true;
+        });
+        const completedTasksCount: number = completedTasks.length;
+
+        const notCompletedTasks: AllTasksAtomType = allTasks.filter((task: TaskAtomType) => {
+            return task.isCompleted === false;
+        });
+        const notCompletedTasksCount: number = notCompletedTasks.length;
+
+        let completedTasksPercentage: number =  0;
+        if (allTasksCount !== 0) {
+            completedTasksPercentage = (completedTasksCount / allTasksCount) * 100;
+        };
+
+        setStats({
+            allCount: allTasksCount,
+            completed: completedTasksCount,
+            notCompleted: notCompletedTasksCount,
+            completedPercentage: completedTasksPercentage
+        });
+
+    }, [allTasks]);
 
     return (
         <div>
             <h1>タスク状況：</h1>
             <ul>
-                <li>Total tasks: {tasksStats.allTasksCount}</li>
-                <li>Tasks Completed: {tasksStats.completedTasksCount}</li>
-                <li>Tasks Not Completed: {tasksStats.notCompletedTasksCount}</li>
-                <li>Percent Completed: {tasksStats.completedTasksPercentage}</li>
+                <li>Total tasks: {stats.allCount}</li>
+                <li>Tasks Completed: {stats.completed}</li>
+                <li>Tasks Not Completed: {stats.notCompleted}</li>
+                <li>Percent Completed: {stats.completedPercentage}%</li>
             </ul>
         </div>
     );
