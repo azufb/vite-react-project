176eeff7d9f62289f101275a835786d458f0768b
azufb <curiousazunas@gmail.com>
2022-12-18 16:47:24 +0900

    グローバル設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Global } from '@emotion/react';
+import { globalStyle } from './styles/globalStyle';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+      <Global styles={globalStyle} />
       <App />
   </React.StrictMode>
 )
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+const globalStyle = css`
+    body {
+        margin: 0;
+    }
+`;
+
+export {
+    globalStyle
+};
