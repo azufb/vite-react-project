1ef5ffaa71ff47e0a94d8f37f0045b9e28d81afa
azufb <curiousazunas@gmail.com>
2022-12-19 09:02:33 +0900

    一覧表示コンポーネント修正

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,32 +1,16 @@
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { allTasksAtom, changeTaskEditableSelector, deleteTaskSelector, changeTaskIsCompletedSelector} from '../recoil/recoilState';
+import { useRecoilValue } from "recoil";
+import { allTasksAtom } from '../recoil/recoilState';
 import { AllTasksAtomType, TaskAtomType } from "../types/recoilStateType";
-import EditForm from "./EditForm";
 
 const List = () => {
     const allTasks = useRecoilValue<AllTasksAtomType>(allTasksAtom);
-    const setDeleteTask = useSetRecoilState<any>(deleteTaskSelector);
-    const setChangeTaskEditable = useSetRecoilState<any>(changeTaskEditableSelector);
-    const setChangeTaskIsCompleted = useSetRecoilState<any>(changeTaskIsCompletedSelector);
 
     return (
         <div>
             {allTasks.map((task: TaskAtomType, index: number) => (
                 <div key={index}>
-                    {task.edit ? (
-                        <EditForm
-                        id={task.id}
-                        title={task.title}
-                    />
-                    ) : (
-                        <>
-                            <span>NO.{task.id}：</span>
-                            <span>{task.title}</span>
-                            <button onClick={() => setChangeTaskIsCompleted(task)}>{task.isCompleted ? 'DONE' : 'TODO'}</button>
-                            <button onClick={() => setChangeTaskEditable(task)}>Edit</button>
-                            <button onClick={() => setDeleteTask(task)}>削除</button>
-                        </>
-                    )}
+                    <span>NO.{task.id}：</span>
+                    <span>{task.title}</span>
                 </div>
             ))}
         </div>
